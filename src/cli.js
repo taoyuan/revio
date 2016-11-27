4d2a0218db81a6e3fed3e23a2b71a4fef22600a9
Yuan Tao <towyuan@outlook.com>
2016-11-27 20:00:22 +0800

    Update

--- src/cli.js
+++ src/cli.js
@@ -11,15 +11,28 @@ module.exports = function () {
 		.option('-c, --config <file>', 'configuration file specifying')
 		.option('-p --port', 'http port');
 
+	program
+		.command('install')
+		.alias('i')
+		.description('install some necessary files to system')
+		.action(() => {
+			require('../scripts/install');
+		});
+
+	program
+		.command('*')
+		.action(() => {
+			const keys = _(program.options)
+				.filter(o => o.long)
+				.map(o => _.replace(o.long, /^[-]*/, ''))
+				.filter(key => !['help', 'version'].includes(key) && !_.isFunction(program[key]))
+				.value();
+
+			run(_.pick(program, keys));
+		});
+
 	program.parse(process.argv);
 
-	const keys = _(program.options)
-		.filter(o => o.long)
-		.map(o => _.replace(o.long, /^[-]*/, ''))
-		.filter(key => !['help', 'version'].includes(key) && !_.isFunction(program[key]))
-		.value();
-
-	run(_.pick(program, keys));
 };
 
 function run(options) {
