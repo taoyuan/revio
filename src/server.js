34a262d5368582c749c14812ef5d41694f0cd9ae
Yuan Tao <towyuan@outlook.com>
2017-01-12 23:11:44 +0800

    Disable the http Agent of the http-proxy library so we force the proxy to close the connection after each request to the backend

--- src/server.js
+++ src/server.js
@@ -118,7 +118,9 @@ class Server {
 			xfwd: (opts.xfwd !== false),
 			prependPath: false,
 			secure: (opts.secure !== false),
-			agent  : http.globalAgent,
+			// Disable the http Agent of the http-proxy library so we force
+			// the proxy to close the connection after each request to the backend
+			agent: false
 			/*
 			 agent: new http.Agent({
 			 keepAlive: true
